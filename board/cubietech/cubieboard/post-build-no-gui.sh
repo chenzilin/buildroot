ca99def276547ec44dd12323a0eac9d379a58c41
chenzilin <chenzilin115@gmail.com>
2015-04-29 13:37:14 +0800

    added cubieboard2 post-build.sh

---
+++ board/cubietech/cubieboard/post-build-no-gui.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# post-build.sh for CubieBoard
+# 2013, Carlo Caione <carlo.caione@gmail.com>
+
+BOARD_DIR="$(dirname $0)"
+
+# modify cmdline prompt in inittab
+echo "export PS1='\u@\h:\w\\$ '" >> $TARGET_DIR/etc/profile
+
+cp -r $BOARD_DIR/rootfs/etc/network/interfaces $TARGET_DIR/etc/network/

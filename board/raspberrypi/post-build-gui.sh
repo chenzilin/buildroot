05cb295e654b90bef20e4792373915163d82db70
chenzilin <chenzilin115@gmail.com>
2015-05-23 12:41:18 +0800

    deleted configs/raspberrypi_gui_own_defconfig

--- board/raspberrypi/post-build-gui.sh
+++ board/raspberrypi/post-build-gui.sh
@@ -6,9 +6,6 @@ echo "export PS1='\u@\h:\w\\$ '" >> $TARGETDIR/etc/profile
 # add eth0 static entry into /etc/network/interfaces
 cp board/raspberrypi/interfaces $TARGETDIR/etc/network/
 
-# change default serial port
-sed -i "s/-L  tty1 115200/-L  ttyAMA0 115200/g" $TARGETDIR/etc/inittab
-
 # change cmdline.txt to kernel
 cp board/raspberrypi/cmdline.txt $TARGETDIR/../images/rpi-firmware/
 
